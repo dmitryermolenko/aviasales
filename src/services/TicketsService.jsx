9382b44e3f6cac29edd6adeb0b1dcc5b2670bcb1
dmitry ermolenko <dmitrijermolenko93@gmail.com>
2020-10-13 09:32:37 +0300

    вынес базовый url в отдельное поле

--- src/services/TicketsService.jsx
+++ src/services/TicketsService.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 export default class TicketsService extends React.Component {
   id = null;
 
+  baseUrl = 'https://front-test.beta.aviasales.ru';
+
   async getTickets() {
     if (!this.id) {
-      const searchIdResponse = await fetch('https://front-test.beta.aviasales.ru/search');
+      const searchIdResponse = await fetch(`${this.baseUrl}/search`);
       const { searchId } = await searchIdResponse.json();
       this.id = searchId;
     }
 
-    const ticketsResponse = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${this.id}`);
+    const ticketsResponse = await fetch(`${this.baseUrl}/tickets?searchId=${this.id}`);
 
     return ticketsResponse.json();
   }
